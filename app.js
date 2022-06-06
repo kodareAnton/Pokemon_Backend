e99a6cbb8971d13ff8974d41ef1a167588c21734
Anton <94136841+kodareAnton@users.noreply.github.com>
2022-06-06 11:58:13 +0200

    början

---
+++ app.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+const mysql = require('mysql2');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+app.locals.con = mysql.createConnection({
+    host: 'localhost',
+    port: '3306',
+    user: 'Pokemon',
+    database: 'pokemon'
+});
+
+app.get('/post', async (req, res) => {
+
+    try{
+    const result = await connection.execute('SELCET * FROM Pokemon')
+    console.log(result);
+    res.json('posts')
+    } catch(err){
+        console.log(err);
+        res.json(err.message)
+    }
+});
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+module.exports = app;
